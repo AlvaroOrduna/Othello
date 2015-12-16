8317a98b7b9ce12958c99294f054b2e8d4b7862d
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2015-12-16 09:19:29 +0100

    Add minmax skeleton
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- src/minimax.py
+++ src/minimax.py
@@ -1,7 +1,16 @@
 # minimax:
 
 
-def minimax(game, maxply):
-    moves = game.generate_moves()
-    return (1, moves[0])
-
+def minimax(game, maxply, algorithm):
+    if algorithm == 0:
+        return first(game)
+    elif algorithm == 1:
+        return random(game)
+    elif algorithm == 2:
+        return guzzler(game)
+    elif algorithm == 3:
+        return minmaxDeep(game, deep)
+    elif algorithm == 4:
+        return minmaxDeepAlphaBeta(game, deep)
+    else:
+        return best(game)
